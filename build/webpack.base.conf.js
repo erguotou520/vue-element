9d7039e645c7b0852410f3e5b8ca8758e7c50e0c
qingwei.li <cinwell.li@gmail.com>
2016-11-29 18:44:01 +0800

    Manual order chunks

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -12,7 +12,8 @@ var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './src/main.js',
+    element: './src/element.js'
   },
   output: {
     path: config.build.assetsRoot,
--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import App from './App'
 
-import Element from 'element-ui'
-Vue.use(Element)
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
